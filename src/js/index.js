ea4e947c25c8a4adb70ee4d039694a0cf2ccc2bf
alexis <rud21003@byui.edu>
2025-01-15 21:11:29 -0700

    formatting

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,14 @@
 const { response } = require("express");
 
 async function updateName() {
-    const nameElement = document.querySelector('.name');
-    const response = await fetch('http://localhost:3000');
-    if (response.ok) {
-        const name = await response.text(); // Shelby Rudy
-        nameElement.textContent = name;
-    }
-    else {
-        nameElement.textContent = 'Error retrieving name';
-    }
+  const nameElement = document.querySelector(".name");
+  const response = await fetch("http://localhost:3000");
+  if (response.ok) {
+    const name = await response.text(); // Shelby Rudy
+    nameElement.textContent = name;
+  } else {
+    nameElement.textContent = "Error retrieving name";
+  }
 }
 
-updateName(); 
+updateName();
